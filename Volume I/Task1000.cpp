3caa11205331219389f3a486c0dbb91c28d97cb4
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-22 22:07:27 +0300

    Added task 1000

---
+++ Volume I/Task1000.cpp
@@ -0,0 +1,13 @@
+/*
+ http://acm.timus.ru/problem.aspx?space=1&num=1000
+ Just input and output
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a,b;
+  cin>>a>>b;
+  cout<<(a+b);
+}
