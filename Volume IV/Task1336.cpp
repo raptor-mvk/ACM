0c2d14cf2d84185f2eeba3b615d2b18464a90768
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-23 00:26:33 +0300

    Added task 1336

---
+++ Volume IV/Task1336.cpp
@@ -0,0 +1,14 @@
+/*
+  http://acm.timus.ru/problem.aspx?space=1&num=1336
+  m could be n^2, k could be n.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	long long n;
+	cin>>n;
+	cout<<(n*n)<<'\n'<<n;
+}
