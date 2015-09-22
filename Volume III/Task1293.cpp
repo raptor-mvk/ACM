dc58d56b430d5705b395b25988d24d75a39f0b9c
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-23 00:40:33 +0300

    Added task 1293

---
+++ Volume III/Task1293.cpp
@@ -0,0 +1,14 @@
+/*
+  http://acm.timus.ru/problem.aspx?space=1&num=1000
+  Just input and output
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n,a,b;
+  cin>>n>>a>>b;
+  cout<<(2*n*a*b);
+}
