9882799fc69cd19e295ddb97be93723971d076c3
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-23 13:14:35 +0300

    Added task 1370

---
+++ Volume IV/Task1370.cpp
@@ -0,0 +1,21 @@
+/*
+  http://acm.timus.ru/problem.aspx?space=1&num=1370
+  Just input and output
+*/
+
+#include <iostream>
+
+using namespace std;
+
+const int DIGITS_COUNT=10;
+
+int main() {
+  int n,m;
+  char *wheel;
+  cin>>n>>m;
+  wheel=new char[n];
+  for(int i=0;i<n;i++)
+    cin>>wheel[i];
+  for(int i=0;i<DIGITS_COUNT;i++)
+    cout<<wheel[(i+m)%n];
+}
