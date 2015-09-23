fefe0878447f41c20f41c2785eb9037eee7d91ef
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-23 11:58:08 +0300

    Added task 1502

---
+++ Volume VI/Task1502.cpp
@@ -0,0 +1,15 @@
+/*
+  http://acm.timus.ru/problem.aspx?space=1&num=1502
+  Each number of points occurred N+2 times, so the answer is
+  (N+2)*(1+2+...+N)=(N+2)*(N+1)*N/2
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long long n;
+  cin>>n;
+  cout<<(((n+2)*(n+1)*n)/2);
+}
