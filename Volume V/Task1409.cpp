c9e654310dd22cb631aaab817c1f3d3dcc568e2f
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-23 11:53:40 +0300

    Added task 1409

---
+++ Volume V/Task1409.cpp
@@ -0,0 +1,14 @@
+/*
+  http://acm.timus.ru/problem.aspx?space=1&num=1409
+  Just input and output
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int a,b;
+  cin>>a>>b;
+  cout<<(b-1)<<' '<<(a-1);
+}
