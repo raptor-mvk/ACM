5570ba44215cfce99fd4457023c2afde9312eb71
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-23 12:43:47 +0300

    Added task 1925

---
+++ Volume X/Task1925.cpp
@@ -0,0 +1,24 @@
+/*
+  http://acm.timus.ru/problem.aspx?space=1&num=1925
+  Just input and output
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n,k,b,g=0,temp;
+	cin>>n>>k;
+	b=k;
+	for(int i=0;i<n;i++) {
+		cin>>temp;
+		b+=temp;
+		cin>>temp;
+		g+=temp;
+	}
+	if(b-g>=2*(n+1))
+		cout<<(b-g-2*(n+1));
+	else
+		cout<<"Big Bang!";
+}
