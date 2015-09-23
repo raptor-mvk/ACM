1ff0ce5de980a79f2d662b01565580fe3e896577
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-23 17:53:31 +0300

    Added task 1026

---
+++ Volume I/Task1026.cpp
@@ -0,0 +1,34 @@
+/*
+  http://acm.timus.ru/problem.aspx?space=1&num=1026
+  Bucket sort
+  #sort
+*/
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+const int DATABASE_CODE=5000;
+
+int main() {
+  int database[DATABASE_CODE];
+  int n,k,t,l,j;
+  cin>>n;
+  memset(database,0,DATABASE_CODE*sizeof(int));
+  for(int i=0;i<n;i++) {
+    cin>>t;
+    database[--t]++;
+  }
+  cin.ignore(1,'\n');
+  cin.ignore(4,'\n');
+  cin>>k;
+  for(int i=0;i<k;i++) {
+    cin>>t;
+    j=0;
+    l=0;
+    while(j<t)
+      j+=database[l++];
+    cout<<l<<'\n';
+  }
+}
