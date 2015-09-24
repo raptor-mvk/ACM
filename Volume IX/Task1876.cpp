0a82969bb9323ded55f1fa5ccaa9844537ed21a7
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-24 12:51:00 +0300

    Added task 1876

--- Volume IX/Task1876.cpp
+++ Volume IX/Task1876.cpp
@@ -1,5 +1,11 @@
 // Topic: arithmetic
 // Subtopic: elementary
+// Idea: the centipede either puts on 40 right slippers at first,
+//       next throws away all remaining right slippers,
+//       next puts on 40 left slippers
+//       or puts on 39 right slippers, next puts on 40 left slippers,
+//       next throws away all remaining left slippers,
+//       next puts on last right slipper
 
 #include <iostream>
 
