a1be7fb3f59355362f2d35abe3c72c9b61c55642
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-24 12:33:52 +0300

    Added task 1876

---
+++ Volume IX/Task1876.cpp
@@ -0,0 +1,12 @@
+// Topic: arithmetic
+// Subtopic: elementary
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << (a * 2 + 39 > b * 2 + 40 ? a * 2 + 39 : b * 2 + 40);
+}
