9a6b6ecd302ce275c0e8f851a26cf7a526dbf642
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-28 14:13:01 +0300

    Added task 1131

---
+++ Volume II/Task1131.cpp
@@ -0,0 +1,21 @@
+// Topic: arithmetic
+// Subtopic: elementary
+// Idea: process simulation
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, k, cur = 1, count = 0;
+	cin >> n >> k;
+	while ((cur < k) && (cur < n)) {
+		cur *= 2;
+		count++;
+	}
+	if (cur < n) {
+		cout << (count + (n - cur) / k + ((n - cur) % k ? 1 : 0));
+	} else {
+		cout << count;
+	}
+}
