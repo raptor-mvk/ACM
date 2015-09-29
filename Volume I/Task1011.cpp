34451044475d40ce41ce783bf2e1b838084db0ba
raptor-mvk <m.v.kamorin@gmail.com>
2015-09-29 17:24:34 +0300

    Added task 1011

---
+++ Volume I/Task1011.cpp
@@ -0,0 +1,23 @@
+// Topic: arithmetic
+// Subtopic: elementary
+// Idea: full search
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+const int MAX_CITIZENS = 10000;
+const double EPS = 5e-5;
+
+int main() {
+  double p, q;
+  cin >> p >> q;
+  for (int i = 1; i <= MAX_CITIZENS; i++) {
+    if ((int)floor((double)i * p / 100.0 + EPS) + 1 <
+        (int)ceil((double)i * q / 100.0 - EPS)) {
+        cout << i;
+        break;
+      }
+  }
+}
