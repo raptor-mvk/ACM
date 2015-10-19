c49dede2495c7e9b611bd7d5ccbfaa29e9270dbc
raptor-mvk <m.v.kamorin@gmail.com>
2015-10-19 10:18:34 +0300

    Added task 1642

--- README.md
+++ README.md
@@ -31,6 +31,7 @@ Solutions to problems from http://acm.timus.ru/
       [1607](http://acm.timus.ru/problem.aspx?space=1&num=1607),
       [1636](http://acm.timus.ru/problem.aspx?space=1&num=1636),
       [1638](http://acm.timus.ru/problem.aspx?space=1&num=1638),
+      [1642](http://acm.timus.ru/problem.aspx?space=1&num=1642),
       [1645](http://acm.timus.ru/problem.aspx?space=1&num=1645),
       [1688](http://acm.timus.ru/problem.aspx?space=1&num=1688),
       [1725](http://acm.timus.ru/problem.aspx?space=1&num=1725),
---
+++ Volume VII/Task1642.cpp
@@ -0,0 +1,36 @@
+// Topic: arithmetic
+// Subtopic: elementary
+
+#include <iostream>
+
+using namespace std;
+
+const int RIGHT = 1001;
+const int LEFT = -1001;
+
+int main() {
+	int n, right, left, exit, cur;
+	cin >> n >> exit;
+	if (!exit) {
+		cout << "0 0\n";
+	} else {
+		left = LEFT;
+		right = RIGHT;
+		for (int i = 0; i < n; i++) {
+			cin >> cur;
+			if (cur < 0 && cur > left) {
+				left = cur;
+			}
+			if (cur > 0 && cur < right) {
+				right = cur;
+			}
+		}
+		if((exit > 0 && exit > right) || (exit < 0 && exit < left)) {
+			cout<<"Impossible\n";
+		}	else if (exit > 0) {
+      cout << exit << ' ' << (exit - 2 * left);
+		}	else {
+			cout << (2 * right - exit) << ' ' << (-exit);
+		}
+	}
+}
